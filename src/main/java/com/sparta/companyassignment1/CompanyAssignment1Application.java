c2aa637a8add327d09905409d3c429995ad735ac
DYKIM\kdy95 <kdy950930@gmail.com>
2024-10-09 15:30:08 +0900

    첫 커밋

---
+++ src/main/java/com/sparta/companyassignment1/CompanyAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.companyassignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAssignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyAssignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/companyassignment1/CompanyAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.companyassignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyAssignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
